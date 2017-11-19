4389eddd56615e0c856f72d89ba1aea58acaed0e
Guilherme Henrique <g.lourenco.santos@gmail.com>
2017-11-19 13:07:09 -0200

    Adding Authentication To Views

--- profileservice/myprofile/api/views.py
+++ profileservice/myprofile/api/views.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
 from myprofile.models import Profile
 from myprofile.api.serializers import ProfileSerializer
+from rest_framework.authentication import (SessionAuthentication,
+                                           BasicAuthentication,
+                                           TokenAuthentication)
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+
 
 class ProfileViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, TokenAuthentication,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
