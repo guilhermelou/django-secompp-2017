d6cd9bbe385867d18fb8be651d41308e5e1d5879
Guilherme Henrique <g.lourenco.santos@gmail.com>
2017-11-19 13:30:30 -0200

    Adding Custom Permissions

--- profileservice/myprofile/api/views.py
+++ profileservice/myprofile/api/views.py
@@ -6,14 +6,18 @@ from rest_framework.authentication import (SessionAuthentication,
                                            TokenAuthentication)
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
+
+from main.api.permissions import IsOwnerOrReadOnly
+
 from rest_framework import mixins
 
 class ProfileViewSet(mixins.ListModelMixin,
                      mixins.RetrieveModelMixin,
                      mixins.UpdateModelMixin,
                      viewsets.GenericViewSet):
+
     authentication_classes = (SessionAuthentication, TokenAuthentication,)
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
